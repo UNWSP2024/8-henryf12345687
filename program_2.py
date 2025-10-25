a8dbe42e1f57c8b885ea6ab30f78996f68ef3aab
henryf12345687 <hjforst@students.unwsp.edu>
2025-10-25 01:09:27 +0000

    update changes

--- program_2.py
+++ program_2.py
@@ -1,4 +1,8 @@
-# Program #2: Word Separator
+# Program #2: 
+# Henry Forst
+#Week 8 programs
+#10/24/2025
+# Word Separator
 # Write a program that accepts as input a sentence in which all of the words are run together, 
 # but the first character of each word is uppercase.  
 # Convert the sentence to a string in which the words are separated by spaces, 
@@ -7,17 +11,23 @@
 
 # Start your changes on line 13
 
-def word_separator(sentence):
-
-    new_sentence = ""
+def word_separator():
+    user_input = input('Enter a sentence with all the words together and the first letter in each word being capatilized: ')
+    new_sentence = user_input[0].upper()
     #    Add your logic here
-
-    return new_sentence.strip()
+    for char in user_input[1:]:
+        if char.isupper():
+            new_sentence += " " + char.lower()
+        else:
+            new_sentence += char
+    new_sentence += "."
+    print(new_sentence)
 
 # Example usage
 
-sentence = "StopAndSmellTheRoses"
+#sentence = "StopAndSmellTheRoses"
 
-new_sentence = word_separator(sentence)
+#new_sentence = word_separator(sentence)
 
-print(new_sentence)
+#print(new_sentence)
+word_separator()
